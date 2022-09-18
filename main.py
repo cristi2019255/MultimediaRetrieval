70a2f890488461520e5ea60b731a15d170c59e82
cristi2019255 <cristi.grosu.2013@mail.ru>
2022-09-18 13:27:22 +0200

    fixing .off for renderer

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 
 from utils.preprocesing import preprocess
-from utils.renderer import render
+from utils.renderer import render, test_render
 
 
 def main():
-    render()
+    render()    
     preprocess()
 
 if __name__ == '__main__':
