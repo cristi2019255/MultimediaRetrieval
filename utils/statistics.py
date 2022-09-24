3d0da3f9d0fdd0330ff741500368e8ce88beee65
cristi2019255 <cristi.grosu.2013@mail.ru>
2022-09-24 13:59:38 +0200

    Fixing histogram plot

--- utils/statistics.py
+++ utils/statistics.py
@@ -21,10 +21,11 @@
 import matplotlib.pyplot as plt
 
 def plot_histogram(data, title = "Histogram of vertex counts"):
+    plt.clf()
     plt.hist(data)
     plt.title(title)
     file_name = title.replace(" ", "_") + ".png"
     plt.savefig(f"./report/{file_name}")   
-    plt.clf()
+    
     #plt.show()
 
