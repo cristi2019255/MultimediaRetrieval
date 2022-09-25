2e14ffb5ccb67f98a91e5bc10fd195a507132940
cristi2019255 <cristi.grosu.2013@mail.ru>
2022-09-25 16:14:45 +0200

    Fixing imports

--- utils/Preprocessor.py
+++ utils/Preprocessor.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from Shape import Shape
 from utils.Logger import Logger
 from utils.tools import *
 from utils.Database import Database
